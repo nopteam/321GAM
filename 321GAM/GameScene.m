7fc2740dbf24cf48d0418ef159630837c91636bb
Máximo De León <maximodeleon@MacBook-Pro.local>
2015-09-06 09:38:09 -0400

    added red circle as the 'character' of the game in the prototype.

--- 321GAM/GameScene.m
+++ 321GAM/GameScene.m
@@ -2,7 +2,7 @@
 //  GameScene.m
 //  321GAM
 //
-//  Created by Leonardo Cascianelli on 06/09/15.
+//  Created by Leonardo ; on 06/09/15.
 //  Copyright (c) 2015 Leonardo Cascianelli. All rights reserved.
 //
 
@@ -12,6 +12,7 @@
 
 -(void)didMoveToView:(SKView *)view {
 	[super didMoveToView:view];
+    [self createCircle: 30.0];
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
@@ -21,6 +22,16 @@
 
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
+}
+
+
+- (void) createCircle: (float) radius{
+    
+    SKShapeNode *circle = [SKShapeNode shapeNodeWithCircleOfRadius:radius];
+    circle.fillColor = [SKColor redColor];
+    circle.lineWidth = 1;
+    circle.position = CGPointMake(self.size.width / 2, self.size.height - 40);
+    [self addChild:circle];
 }
 
 @end
